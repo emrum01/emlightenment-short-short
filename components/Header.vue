a6927a5d7fc2d40fe1009a0896168187ea41bb19
emrum01 <emrum01@gmail.com>
2021-05-02 21:19:39 +0900

    feat:ヘッダーのレスポンシブ対応をした

--- components/Header.vue
+++ components/Header.vue
@@ -40,75 +40,11 @@ export default {
     background-color: #3f3934;
   }
 
-  .test {
-    font-family: 'HitchhikerFont';
-  }
-
-  .empty {
-    height: 80px;
-    margin-bottom: 40px;
-  }
-
   .logo {
     display: flex;
     font-family: 'HitchhikerFont';
     justify-content: center;
     color: #e68048;
-  }
-
-  .logoImg {
-    height: 28px;
-  }
-
-  .menuBtn {
-    display: none;
-  }
-
-  .menu {
-    display: flex;
-    padding: 12px 0;
-  }
-
-  .lists {
-    display: flex;
-    align-items: center;
-
-    &:first-child::after {
-      content: '';
-      width: 1px;
-      height: 30px;
-      background-color: var(--color-text-off);
-      margin-right: 40px;
-    }
-  }
-
-  .list {
-    margin-right: 30px;
-    white-space: nowrap;
-
-    &.noMargin {
-      margin: 0;
-    }
-
-    a,
-    a:visited {
-      color: var(--color-text-main);
-
-      &.signup {
-        border-radius: 5px;
-        background: linear-gradient(to right bottom, #5630af, #3067af);
-        color: #fff;
-        text-align: center;
-        padding: 8px 24px;
-      }
-
-      &.signin {
-        border-radius: 5px;
-        background-color: var(--color-bg-purple);
-        text-align: center;
-        padding: 8px 24px;
-      }
-    }
   }
 }
 
@@ -120,100 +56,18 @@ export default {
     right: 0;
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    background-color: #fff;
+    justify-content: center;
+    background-color: #3f3934;
     padding: 16px;
     z-index: 10;
     border-bottom: 1px solid var(--color-border);
     font-family: 'HitchhikerFont';
   }
-  .empty {
-    height: 61px;
-  }
-
   .logo {
-    display: inline-block;
-    height: 56px;
-  }
-
-  .logoImg {
-    height: 56px;
-  }
-
-  .menuBtn {
-    border: none;
-    background: none;
-    margin: 0;
-    padding: 0;
-    cursor: pointer;
-  }
-
-  .menu {
-    position: absolute;
-    left: 0;
-    top: 61px;
-    display: none;
-    flex-direction: column-reverse;
-    width: 100%;
-    background-color: #fff;
-    border-bottom: 1px solid var(--color-border);
-    z-index: 2001;
-    padding-top: 8px;
-
-    &.isOpen {
-      display: flex;
-    }
-  }
-
-  .lists {
-    padding: 8px 0;
-
-    &:first-child {
-      padding-top: 0;
-    }
-  }
-
-  .list {
-    padding: 0 16px;
-    white-space: nowrap;
-
-    a {
-      display: block;
-      color: var(--color-text-main);
-      padding: 8px 0;
-      border-bottom: 1px solid var(--color-border-light);
-
-      &.signup {
-        border-radius: 5px;
-        background: linear-gradient(to right bottom, #5630af, #3067af);
-        color: #fff;
-        text-align: center;
-        font-weight: bold;
-      }
-
-      &.signin {
-        display: block;
-        border-radius: 5px;
-        background-color: var(--color-bg-purple);
-        text-align: center;
-        font-weight: bold;
-        margin-bottom: 12px;
-      }
-    }
-
-    &:last-child a {
-      border-bottom: none;
-    }
-  }
-
-  .mask {
-    position: fixed;
-    top: 61px;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    z-index: 2000;
+    display: flex;
+    font-family: 'HitchhikerFont';
+    justify-content: center;
+    color: #e68048;
   }
 }
 </style>
